090376344dd95397346b33c954f4f8eb0d50d49a
vectorsigmaissomewhere <anishchy501625@gmail.com>
2026-02-25 10:24:04 +0545

    changed path of user model in settings 2

--- server/home/home/settings.py
+++ server/home/home/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'anymail',
 ]
 
-AUTH_USER_MODEL = 'home.accounts.User'
+AUTH_USER_MODEL = 'home.accounts.models.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

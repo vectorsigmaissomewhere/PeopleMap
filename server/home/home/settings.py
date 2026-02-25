bde845038db090be7c0276ae2ab179123af6cd66
vectorsigmaissomewhere <anishchy501625@gmail.com>
2026-02-25 10:59:10 +0545

    making some changes in settings

--- server/home/home/settings.py
+++ server/home/home/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'anymail',
 ]
 
-AUTH_USER_MODEL = 'accounts.apps.AccountsConfig.User'
+AUTH_USER_MODEL = 'AccountsConfig.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

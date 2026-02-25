c6000e36306eccea5ca224f71f948f69749101dd
vectorsigmaissomewhere <anishchy501625@gmail.com>
2026-02-25 21:43:15 +0545

    ;changing settings path in wsgi

--- server/home/home/wsgi.py
+++ server/home/home/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.home.settings')
 
 application = get_wsgi_application()

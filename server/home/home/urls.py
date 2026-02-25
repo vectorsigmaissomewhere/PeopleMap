306639d58d005e13dcf4fa711aea0d2c3723b083
vectorsigmaissomewhere <anishchy501625@gmail.com>
2026-02-25 10:37:02 +0545

    testing the application

--- server/home/home/urls.py
+++ server/home/home/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+def ping(request):
+    return JsonResponse({"status": "ok", "message": "API base is working"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('accounts.urls')),
     path('api/people/', include('people.urls')),
+    path('ping/', ping),
 ]
